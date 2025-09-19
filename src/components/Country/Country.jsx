aaad441abaf7be492b713cc14576792b0de71bc2
Ayub <md.ayub0070@gmail.com>
2025-09-20 01:44:10 +0600

    commit

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+import './Country.css'
+
+const Country = ({country, handleVisitedCountries, handleVisitedFlag}) => {
+    const[visited, setVisited] = useState(false);
+    
+
+    // console.log(country)
+
+    const handleVisited = ()  => {
+        // basic systen
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // secound system
+        // setVisited(visited ? false : true)
+
+        // 3rd systen
+        setVisited(!visited)
+        handleVisitedCountries(country)
+    }
+    
+    return (
+        <div className={`country ${visited && `country-visited`}`}>
+            
+            <img src={country?.flags?.flags?.png} alt={country.flags.flags.png.alt} />
+            <h3>Name:{country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            
+            {/* <p>Region : {country.region.region} </p> */}
+            {/* <p>Capital: {country.capital.capital}</p> */}
+            <p>Area : {country.area.area} 
+                {country.area.area > 300000 ? "Big Country" :  'Small Country'} </p>
+                <button onClick={handleVisited}>
+                    {visited ? 'Visited' : 'Not Visited'}</button>
+                    <button onClick={() => { handleVisitedFlag(country?.flags?.flags?.png) }}>Add Visited Flags</button>
+        </div>
+    );
+};
+
+export default Country;
